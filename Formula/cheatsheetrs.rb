f06f612fabd4de392d2a4c74b988777401f193fd
jheryer <jheryer@gmail.com>
2023-05-07 11:05:29 -0500

    0.1.1

--- Formula/cheatsheetrs.rb
+++ Formula/cheatsheetrs.rb
@@ -1,16 +1,15 @@
-require 'fileutils'
 class Cheatsheetrs < Formula
     desc "cheatsheet is a command-line tool written in Rust that lets users quickly view cheat sheets on the command line."
     homepage "https://github.com/jheryer/cheatsheetrs"
-    version "0.1.0"
+    version "0.1.1"
     license "MIT License"
- 
+
     if Hardware::CPU.intel?
-      url "https://github.com/jheryer/cheatsheetrs/releases/download/v0.1.0/cheatsheet-0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "c096407eb86a71d6bb196d97967a728f7dd2f6f183f9be5518cf4d7786b4625f"
+      url "https://github.com/jheryer/cheatsheetrs/releases/download/0.1.1/cheatsheet-0.1.1-x86_64-apple-darwin.tar.gz"
+      sha256 "4bac93fffdb082dd6776ff822e8edc6d187a6b5ab6ed9322893b416cd18d703c"
     elsif Hardware::CPU.arm?
-      url "https://github.com/jheryer/cheatsheetrs/releases/download/v0.1.0/cheatsheet-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "8205cf60a0d8f435f739d698f6ce2c98459b0feedb265fa70f596a8a2cce6068"
+      url "https://github.com/jheryer/cheatsheetrs/releases/download/0.1.1/cheatsheet-0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "969a7db6334049e465bddab8a5210f0588787ff488dd777638e1a085fde565b4"
     end 
 
     def install
@@ -21,13 +20,13 @@ class Cheatsheetrs < Formula
     end
   
     def post_install
-      ohai "Sheets location: CHEAT_SHEET_PATH=#{HOMEBREW_PREFIX}/Cellar/#{name}/#{version}/sheets"
+      ohai "Sheets location: CHEAT_SHEET_PATH=#{prefix}/sheets"
     end
 
     private def create_wrapper
       wrapper = "#!/usr/bin/env bash
-      export CHEAT_SHEET_PATH=\"${CHEAT_SHEET_PATH:-/opt/homebrew/Cellar/cheatsheetrs/0.1.0/sheets}\"
-      #{HOMEBREW_PREFIX}/Cellar/#{name}/#{version}/libexec/cheatsheet \"$@\""
+      export CHEAT_SHEET_PATH=\"$\{CHEAT_SHEET_PATH:-#{prefix}/sheets\}\"
+      #{prefix}/libexec/cheatsheet \"$@\""
       File.write('cheatsheet_wrapper',wrapper)
     end
 
@@ -35,3 +34,4 @@ class Cheatsheetrs < Formula
       system "#{bin}/cheatsheet", "--version"
     end
   end
+ 
